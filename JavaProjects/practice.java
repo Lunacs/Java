b5d6e04feff9868ff5fad1a5e8557a0c3a28b0ea
Mr. Robot <83933378+Lunacs@users.noreply.github.com>
2022-06-11 23:43:26 +0800

    .

---
+++ JavaProjects/practice.java
@@ -0,0 +1,16 @@
+package JavaProjects;
+
+/**
+ * @(#)practice.java
+ *
+ *
+ * @author 
+ * @version 1.00 2021/9/14
+ */
+
+public class practice {
+    public static void main(String[] args) {
+        System.out.println ("Hello World");
+   		system.out.printIn.nextLine;
+    }
+}
