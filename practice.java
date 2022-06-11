2c86d6828e9236151a701c856d4adc43ba357502
Mr. Robot <83933378+Lunacs@users.noreply.github.com>
2022-06-11 23:20:31 +0800

    Revert "."
    This reverts commit 0eda8d5a0fb332f40f3b56e7d9c1d3fa310ec70a.

---
+++ practice.java
@@ -0,0 +1,14 @@
+/**
+ * @(#)practice.java
+ *
+ *
+ * @author 
+ * @version 1.00 2021/9/14
+ */
+
+public class practice {
+    public static void main(String[] args) {
+        System.out.println ("Hello World");
+   		system.out.printIn.nextLine;
+    }
+}
